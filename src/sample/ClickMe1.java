28c58190a1680a9ffa85b49100e0556dfb785f16
Faxar <alazerus@gmail.com>
2018-05-08 23:34:37 +0300

    Initial commit

---
+++ src/sample/ClickMe1.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class ClickMe1 extends Application {
+
+    Button btn;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        btn = new Button();
+        btn.setText("Click Me!");
+        BorderPane pane = new BorderPane();
+        pane.setCenter(btn);
+        Scene scene = new Scene(pane, 300, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
